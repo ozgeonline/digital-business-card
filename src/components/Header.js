9d509734c29f4ec9d0cf3d8f40470941649d0aae
ozgeonline <nisroc.414@hotmail.com>
2023-08-14 17:21:46 +0300

    my digital card

---
+++ src/components/Header.js
@@ -0,0 +1,8 @@
+import headerImg from '../images/123.png'
+function Header(){
+  return(
+    <img src={headerImg} alt="Header İmage" className="header-img" />
+  )
+}
+
+export default Header;
---
+++ src/components/Main.js
@@ -0,0 +1,39 @@
+function Main(){
+  return(
+    <div className="main-content">
+      <h1>Özge Kocabaşoğlu</h1>
+      <h3>Jr. Web Developer</h3>
+      <h4>nisroc.414@hotmail.com</h4>
+      <div className="btn-group">
+        <a className="btn btn-email" href="mailto:nisroc.414@hotmail.com">
+          <i class="fa-solid fa-envelope"></i>Email
+        </a>
+        <a className="btn btn-linkedin" href="https://www.linkedin.com/in/ozgeonline/"  target="_blank" rel="noreferrer">
+          <i class="fa-brands fa-linkedin"></i>LinkedIn
+        </a>
+      </div>
+      <div className="main-text">
+        <h2>About</h2>
+        <p>I am a frontend developer with a particular interest in making things simple and automating daily tasks. I try to keep up with security and best practices, and am always looking for new things to learn.</p>
+        <h2>Interests</h2>
+        <p>Food expert. Music scholar. Reader. Internet fanatic. Bacon buff. Entrepreneur. Travel geek. Pop culture ninja. Coffee fanatic.</p>
+      </div>
+      <footer>
+        <a href="https://twitter.com/ozgeonline0"  target="_blank" rel="noreferrer">
+          <i class="fa-brands fa-twitter"></i>
+        </a>
+        <a href="https://www.facebook.com/ozgeonline0o"  target="_blank" rel="noreferrer">
+          <i class="fa-brands fa-facebook-f"></i>
+        </a>
+        <a href="https://www.instagram.com/ozgeonline0/"  target="_blank" rel="noreferrer">
+          <i class="fa-brands fa-instagram"></i>
+        </a>
+        <a href="https://github.com/ozgeonline"  target="_blank" rel="noreferrer">
+          <i class="fa-brands fa-github"></i>
+        </a>
+      </footer>
+    </div>
+  )
+}
+
+export default Main;
